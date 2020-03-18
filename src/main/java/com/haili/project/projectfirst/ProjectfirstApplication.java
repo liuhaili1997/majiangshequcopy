e8ef96ab94045f647154f7afdcdce42135dd31e0
liuhaili <haili199703@126.com>
2020-03-18 18:06:02 +0800

    初始化环境和与远程仓库建立连接

---
+++ src/main/java/com/haili/project/projectfirst/ProjectfirstApplication.java
@@ -0,0 +1,13 @@
+package com.haili.project.projectfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haili/project/projectfirst/ProjectfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haili.project.projectfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
